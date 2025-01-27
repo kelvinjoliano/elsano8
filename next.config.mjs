86b673f4fc65c3ef4d0cbf134cb27b7768493206
kelvinjoliano <kelvinjoliano18@gmail.com>
2025-01-27 20:32:38 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true, // Aktifkan folder "app" jika dipakai
+    },
     images: {
         remotePatterns: [
             {
